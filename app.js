1802f26135142d0cd8786ea8802705aaee007d13
gautamRaj-cloud <raj350199@gmail.com>
2021-10-02 11:11:46 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+const add = require("./utils.js");
+const notes = require("./notes");
+const sum = add(4, 6);
+console.log(sum);
+const message = notes();
+console.log(message);
---
+++ utils.js
@@ -0,0 +1,7 @@
+console.log("Utils.js is called");
+const name = "Raj ";
+
+const add = function (a, b) {
+  return a + b;
+};
+module.exports = add;
