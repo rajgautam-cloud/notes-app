f047d78b5da98c2ef11d1619d6dffa8bbd4e11ee
gautamRaj-cloud <raj350199@gmail.com>
2021-10-03 11:05:30 +0530

    first commit

--- app.js
+++ app.js
@@ -1,6 +1,42 @@
-const add = require("./utils.js");
+const chalk = require("chalk");
+const yargs = require("yargs");
 const notes = require("./notes");
-const sum = add(4, 6);
-console.log(sum);
-const message = notes();
-console.log(message);
+
+// create add command
+yargs.command({
+  command: "add",
+  description: "Add a new note",
+  builder: {
+    title: {
+      describe: "Note title",
+    },
+  },
+  handler: function (argv) {
+    console.log("Adding a new note!", argv);
+  },
+});
+//create remove command
+yargs.command({
+  command: "remove",
+  description: "Remove an existing note",
+  handler: function () {
+    console.log("Removing note");
+  },
+});
+//create list command
+yargs.command({
+  command: "list",
+  description: "Listing the notes",
+  handler: function () {
+    console.log("Listing note");
+  },
+});
+//create read command
+yargs.command({
+  command: "read",
+  description: "read an existing note",
+  handler: function () {
+    console.log("reading note");
+  },
+});
+console.log(yargs.argv);
